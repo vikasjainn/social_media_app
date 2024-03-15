a407ad8fc72c95fcd21d958d2de2a6a74cba9cc9
Vikas Jain <jainv6644@gmail.com>
2024-03-15 16:33:40 +0530

    Update users.rb

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
         name { Faker::Name.first_name }
         username { Faker::Internet.username }
         email { Faker::Internet.email }
-        password { 'Vipul@403' }
+        password { 'Vikas@123' }
         password_digest { BCrypt::Password.create(password) }
         profile_pic {nil}
         cover_pic {nil}
